078da271741fe84a9e15997599a10f7f5f8df231
Igor Cescon de Moura <igorcmgamer@gmail.com>
2020-02-13 11:07:05 -0300

    replace character for virtual youtubers

---
+++ src/js/data/2020-02-13.js
@@ -0,0 +1,893 @@
+dataSetVersion = "2020-02-13"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Agency",
+    key: "agency",
+    tooltip: "Check this to restrict to certain agencies.",
+    checked: false,
+    sub: [
+      { name: "Hololive (ホロライブ)", key: "hololive" },
+      { name: "Nijisanji (にじさんじ)", key: "nijisanji" },
+    ]
+  },
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Tokino Sora",
+    img: "EkkljRm.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Roboco",
+    img: "SKOT3s8.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Sakura Miko",
+    img: "JO21EvU.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Akai Haato",
+    img: "LBShW8y.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Yozora Mel",
+    img: "dJ9CGHp.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Natsuiro Matsuri",
+    img: "uEeWw7y.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Aki Rosenthal",
+    img: "43NO6sC.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Shirakami Fubuki",
+    img: "WxqLOr3.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Oozora Subaru",
+    img: "bQ9loIN.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Yuzuki Choco",
+    img: "tfKf1PP.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Murasaki Shion",
+    img: "JnjgjIH.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Nakiri Ayame",
+    img: "kKE2YXU.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Minato Aqua",
+    img: "JHezCPy.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Ookami Mio",
+    img: "shkpGS9.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Nekomata Okayu",
+    img: "Vpmmit8.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Inugami Korone",
+    img: "oAdQEQP.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "AZKi",
+    img: "ebVsilD.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Hoshimachi Suisei",
+    img: "QEUu5KZ.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Usada Pekora",
+    img: "GXqxhjk.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Uruha Rushia",
+    img: "EFU18LJ.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Shiranui Flare",
+    img: "FnmF3La.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Shirogane Noel",
+    img: "cjvCTAd.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Houshou Marine",
+    img: "qlaYSNc.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Tsunomaki Watame",
+    img: "qeoByAh.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Tokoyami Towa",
+    img: "ucabEkP.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Kiryu Coco",
+    img: "1RLf2jf.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Amane Kanata",
+    img: "uBPxTsR.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Himemori Luna",
+    img: "nkm0s2H.png",
+    opts: {
+      agency: ['hololive']
+    }
+  },
+  {
+    name: "Tsukino Mito",
+    img: "8CjmWen.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Honma Himawari",
+    img: "mIxhWJd.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Sasaki Saku",
+    img: "2G3ryuZ.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Otogibara Era",
+    img: "Zr9MdAY.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Shiina Yuika",
+    img: "FZkrxWw.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Higuchi Kaede",
+    img: "fDRjMLj.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Shizuka Rin",
+    img: "I51FoFd.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Yuzuki Roa",
+    img: "HKCxRow.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Lize Helesta",
+    img: "tSiDDBM.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Ryushen",
+    img: "8RybFJJ.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Suzuka Utako",
+    img: "4puVJZN.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Ange Katrina",
+    img: "rqMriXR.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Kuzuha",
+    img: "unEUPpg.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Suzuhara Lulu",
+    img: "KLTVZW3.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Ars Almal",
+    img: "vgaZjBn.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Belmond Banderas",
+    img: "KD7RNlJ.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Inui Toko",
+    img: "Z55ozCT.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Rindou Mikoto",
+    img: "VDjl0eK.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Kanae",
+    img: "4zuHGQn.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Gundo Mirei",
+    img: "0wHSeSi.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Yashiro Kizuku",
+    img: "Oh23qlA.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Sister Claire",
+    img: "YDAlKCM.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Takamiya Rion",
+    img: "r2nQHl4.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Dola",
+    img: "7Y7gmdD.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Mononobe Alice",
+    img: "w1iEQst.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Mayuzumi Kai",
+    img: "Leh0Lio.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Yuuki Chihiro",
+    img: "XMG5Qyr.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Morinaka Kazaki",
+    img: "vjO8on3.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Ex Albio",
+    img: "unQeC0q.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Warabeda Meiji",
+    img: "UkYvTd7.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Kagami Hayato",
+    img: "n4KjGW3.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Debidebi Debiru",
+    img: "1TPSHUf.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Joe Rikiichi",
+    img: "dOnjiXo.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Yorumi Rena",
+    img: "qbFKCaL.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Kenmochi Toya",
+    img: "1MARerM.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Hanabatake Chaika",
+    img: "91j7KNp.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Elu",
+    img: "2KxuOqE.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Sukoya Kana",
+    img: "OP4NE6f.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Maimoto Keisuke",
+    img: "ZrSgGDa.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Yumeoi Kakeru",
+    img: "Hjajq62.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Ushimi Ichigo",
+    img: "iBmPhXL.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Nui Sociere",
+    img: "EfEwdzF.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Hoshikawa Sara",
+    img: "z1nOSsG.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Saegusa Akina",
+    img: "09mJs2A.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Sakura Ritsuki",
+    img: "0k8UYXe.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Hakase Fuyuki",
+    img: "WPAOcIt.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Ienaga Mugi",
+    img: "GjBa9KJ.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Uzuki Kou",
+    img: "umxTkV6.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Makaino Ririmu",
+    img: "I8pQoW5.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Emma August",
+    img: "PZozt0K.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Kanda Shoichi",
+    img: "43oZykC.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Yuhi Riri",
+    img: "CC80W8w.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Aiba Uiha",
+    img: "V7mVnOx.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Moira",
+    img: "SxdkJDE.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Shellin Burgundy",
+    img: "OIeyCj9.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Amemori Sayo",
+    img: "ItiFibs.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Aizono Manami",
+    img: "BC1DbeF.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Kuroi Shiba",
+    img: "QcBH7OP.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Akabane Youko",
+    img: "kdjbjVa.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Ratna Petit",
+    img: "Jx1U3Ef.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Hayama Marin",
+    img: "k6jSdCn.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Amamiya Kokoro",
+    img: "oFtHKzC.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Mashiro",
+    img: "Q6P8Avc.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Fuwa Minato",
+    img: "RFjF3IX.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Hayase Sou",
+    img: "VgoxEJt.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Eli Conifer",
+    img: "gh1aFML.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Machita Chima",
+    img: "LmuN8ou.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Fumino Tamaki",
+    img: "gXtTCKX.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Izumo Kasumi",
+    img: "MhZTm33.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Matsukai Mao",
+    img: "ZJ6Wlbu.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Shirayuki Tomoe",
+    img: "qrxxzgb.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Fumi",
+    img: "AtNwMyt.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Levi Elipha",
+    img: "Bp0XQPb.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Fushimi Gaku",
+    img: "dmkCu3o.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Onomachi Haruka",
+    img: "iD1QCsI.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Yukishiro Mahiro",
+    img: "L9JMXzq.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Suzuki Masaru",
+    img: "wY00VK5.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Luis Cammy",
+    img: "BP80HdO.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Todoroki Kyoko",
+    img: "MVRazkw.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Seto Miyako",
+    img: "jw5LHPe.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Suzuya Aki",
+    img: "DwtNHkq.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Naraka",
+    img: "BhVrBzC.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Furen E Lustario",
+    img: "kZDwaXO.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Shibuya Hajime",
+    img: "YBErvbj.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Gilzaren Ⅲ",
+    img: "XPT54hH.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Achikita Chinami",
+    img: "Wo2WU36.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Asuka Hina",
+    img: "xYLaRH2.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Gwelu Os Gar",
+    img: "TfsZB3d.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Yaguruma Rine",
+    img: "L1QAfYh.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Yamagami Karuta",
+    img: "i7eNFtK.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Azuchi Momo",
+    img: "uAOwBD1.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Kurusu Natsume",
+    img: "ACUgbBL.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Melissa Kinrenka",
+    img: "DAIDQId.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Ibrahim",
+    img: "wyMwdev.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Harusaki Air",
+    img: "9s4ZtaZ.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Kataribe Tsumugu",
+    img: "32nT6sV.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+  {
+    name: "Naruse Naru",
+    img: "ERDa803.png",
+    opts: {
+      agency: ['nijisanji']
+    }
+  },
+];
